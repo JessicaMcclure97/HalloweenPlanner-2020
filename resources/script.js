8510327a043f3918463c4993e4af55add602cbde
Andrew Barron <awmb1@st-andrews.ac.uk>
2020-10-31 14:42:55 +0000

    UI improvements, form submits with AJAX now

--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,12 @@
-let peopleCount = document.getElementById("peopleCount");
+const peopleCount = document.getElementById("peopleCount");
 peopleCount.addEventListener("input", function () {
     // Hide all name inputs and make not-required
     Array.from(document.getElementsByClassName("name-input")).forEach(function (item) {
         item.style.display = "none";
         item.removeAttribute("required");
     })
+
+    document.getElementById("peopleCountInvalid").style.display = "block";
 
     function displayAndRequire(element) {
         element.style.display = "block";
@@ -24,6 +26,39 @@ peopleCount.addEventListener("input", function () {
             displayAndRequire(document.getElementById("name1"));
         case "1":
             displayAndRequire(document.getElementById("name0"));
+            document.getElementById("peopleCountInvalid").style.display = "none";
+    }
+});
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (form.checkValidity()) {
+        // empty any name fields which aren't included in the number of participants
+        switch (peopleCount.value) {
+            case "1":
+                document.getElementById("name1").value = "";
+            case "2":
+                document.getElementById("name2").value = "";
+            case "3":
+                document.getElementById("name3").value = "";
+            case "4":
+                document.getElementById("name4").value = "";
+            case "5":
+                document.getElementById("name5").value = "";
+        }
+
+        // submit the form
+        $.ajax({
+            type: "POST",
+            url: "api/plan",
+            data: $("#form").serialize(), // serializes the form's elements.
+            success: function(data)
+            {
+                alert(data); // show response from the php script.
+            }
+        });
     }
 });
 
