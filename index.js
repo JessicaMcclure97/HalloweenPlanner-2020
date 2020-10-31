55de8f43951a7c45b4e0e8d68da2b82a0c03e783
Jessica McClure <58782366+JessicaMcclure97@users.noreply.github.com>
2020-10-31 13:04:37 +0000

    andrew made me

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express = require('express');
 const session = require('express-session');
 
@@ -7,7 +9,7 @@ const port = 22224;
 app.use(session({secret:'0b1f608ad333a1911f6851e2c2a183496739fecc36cffe347d4f1c563319c8d0f0d1bac720267863b4e0f9ab928a1ecd40e7d93aeaf65d5e79479b3278b9894b'}));
 let ssn;
 
-app.post('/login', async function (req,res) {
+app.post('/api/login', async function (req,res) {
     ssn=req.session;
 
     if (!ssn.name) {
@@ -17,7 +19,63 @@ app.post('/login', async function (req,res) {
         res.send("Logged in with ID " + ssn.name);
     }
 });
+app.post('/api/plan', function (req,res) {
+    let html = "";
+    console.log('post: ' + req.url);
+    req.setEncoding('utf8');
+    req.on('data', chunk => {
+        console.log('Got a line of post data: ', chunk);
+        html = processForm( chunk, res ); //Server side validation
+    })
+
+    req.on('end', () => {
+        console.log('End of Data - sending reply');
+        if (html === "good"){
+            writeJSON();
+        }else{
+            //unsuccessul validation
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(generateOrderHTML());
+            res.write(String(html));
+            res.end();
+        }
+    })
+
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
+
+let formInputs = new Map();
+
+//validating form
+function processForm(chunk, res){
+    var objects[] = chunk.split("&");
+
+    for(var i = 0; i < objects.length-1; i++){ //-1 since submit button will always be there
+        var info = objects[i].split("=");
+        info[1] = info[1].replace(/[+]/g, ' ');
+        //validating fields
+        if(false/*!formValidation(info[0], info[1])*/){ //form validation later
+            return "<script>alert(\"Cannot Process form. Please press the " +
+                "back button to return to the order form.\");</script>";
+        }else{
+            formInputs.set(info[0], info[1]);
+        }
+    }
+
+    return "good";
+}
+
+function writeJSON(){
+
+    var obj = "{";
+    for (const [key, value] of formInputs.entries()) {
+       obj += "" +key+ ":" + value+",";
+    }
+    obj += "}";
+    let data = JSON.stringify(student);
+    fs.writeFile('eventPlan.json', data);
+}
+
